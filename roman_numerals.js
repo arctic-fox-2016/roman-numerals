029f1880a81e11c6fa7ac7a77a02f74ca3ed5ae4
Toni <tony_chen93@yahoo.com>
2016-08-29 17:52:40 +0700

    Roman numerals

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,9 +1,32 @@
 function to_roman_old(n) {
-  // your code here
+  let array_of_roman = ["M", "CM", "D", "CD", "C", "L", "XL", "XVI", "X", "VIIII", "V", "IIII", "I"];
+  let array_of_decimal = [1000, 900, 500, 400, 100, 50, 40, 16, 10, 9, 5, 4, 1];
+
+  let result = ''
+  // n = 16
+  for(let i = 0; i < array_of_decimal.length; i++) {
+    while(n >= array_of_decimal[i]) {
+      result += array_of_roman[i]
+      n -= array_of_decimal[i]
+    }
+  }
+  return result;
+
 }
 
 function to_roman(n) {
-  // your implementation code here
+  let array_of_roman = ["M", "CM", "D", "CD", "C", "L", "XL", "XIV", "X", "IX", "V", "IV", "I"];
+  let array_of_decimal = [1000, 900, 500, 400, 100, 50, 40, 14, 10, 9, 5, 4, 1];
+
+  let result = ''
+  // n = 16
+  for(let i = 0; i < array_of_decimal.length; i++) {
+    while(n >= array_of_decimal[i]) {
+      result += array_of_roman[i]
+      n -= array_of_decimal[i]
+    }
+  }
+  return result;
 }
 
 // Drive code
@@ -12,8 +35,8 @@ console.log("input | expected | actual");
 console.log("———|—————|———");
 console.log("4     | IIII     | ", to_roman_old(4));
 console.log("9     | VIIII    | ", to_roman_old(9));
-console.log("13    | XIII     | ", to_roman_old(13));
-console.log("1453  | MCDLIII  | ", to_roman_old(1453));
+console.log("16     | XVI    | ", to_roman_old(16));
+console.log("1453  | MCCCCCLIII  | ", to_roman_old(1453));
 console.log("1646  | MDCXLVI  | ", to_roman_old(1646));
 
 console.log("My totally sweet testing script for new roman\n");
